406e2f9d56ce5f91822c3243c2c4e3aef8cf00b2
MakotoAtsu <chris03152008@gmail.com>
2022-11-26 01:09:18 +0800

    fix response status error

--- src/models/events/pubSubEvent.ts
+++ src/models/events/pubSubEvent.ts
@@ -1,8 +1,22 @@
+/**
+ * GCP Pub Sub EventAttributes
+ * @typedef {object} EventAttributes
+ * @property {string} eventType
+ * @property {string} application
+ */
 export interface EventAttributes {
   eventType: string;
   application: string;
 }
 
+/**
+ * GCP Pub Sub EventMessage Model
+ * @typedef {object} EventMessage
+ * @property {string} publishTime
+ * @property {string} messageId
+ * @property {string} data
+ * @property {EventAttributes} attributes
+ */
 export interface EventMessage {
   attributes: EventAttributes;
   data: string;
@@ -10,6 +24,12 @@ export interface EventMessage {
   publishTime: string;
 }
 
+/**
+ * GCP Pub Sub Event model
+ * @typedef {object} PubSubEvent
+ * @property {string} subscription
+ * @property {EventMessage} message
+ */
 export interface PubSubEvent {
   message: EventMessage;
   subscription: string;
